126ec530e881c0ed9b53ad8bd54744eaeb688203
Asrar Ahammad <asrarahammadshaik@gmail.com>
2026-06-26 07:44:32 +0530

    style: update mobile navigation border radius to full in DashboardLayout

--- momnts-web/src/layouts/DashboardLayout.tsx
+++ momnts-web/src/layouts/DashboardLayout.tsx
@@ -254,7 +254,7 @@ const DashboardLayout = () => {
         }}
         className="fixed left-1/2 -translate-x-1/2 z-50 lg:hidden w-[85%] sm:w-[70%] md:w-[50%] max-w-[360px]"
       >
-        <nav className="flex items-center justify-around bg-background/80 backdrop-blur-md border border-neutral-200/50 dark:border-white/10 rounded-[28px] p-2 shadow-2xl ring-1 ring-black/5 dark:ring-white/5">
+        <nav className="flex items-center justify-around bg-background/80 backdrop-blur-md border border-neutral-200/50 dark:border-white/10 rounded-full p-2 shadow-2xl ring-1 ring-black/5 dark:ring-white/5">
           {navItems.map((item) => (
             <button
               key={item.title}
